063dc866fa4ad25f2651a0bba10398178da6955a
nhunglth <hongnhungle.hn@gmail.com>
2023-10-20 21:52:14 +0700

    ok

--- interfaces/PageUIs/LoginBaseUI.java
+++ interfaces/PageUIs/LoginBaseUI.java
@@ -2,4 +2,5 @@ package PageUIs;
 
 public class LoginBaseUI {
 	public static final String EMAIL_TEXTBOX  = "//input[@id='Email']";
+	public static final String EMAIL_TEXTBOXq  = "//input[@id='Email']";
 }
